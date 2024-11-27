cdf348119a3bb554a029a28a95252654744a6517
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-11-28 01:20:15 +0700

    feat: draw NPC

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,42 +5,43 @@ import entities.Entity;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel implements  Runnable{
+public class GamePanel extends JPanel implements Runnable {
 
     final int originalTileSize = 32;
     int scale = 2;
 
-//    TILE SETTING
+    //    TILE SETTING
     public final int tileSize = originalTileSize * scale;
     public final int maxScreenCol = 16;
     public final int maxScreenRow = 12;
     public final int screenHeight = tileSize * maxScreenRow;
     public final int screenWidth = tileSize * maxScreenCol;
 
-//    WORLD SETTING
+    //    WORLD SETTING
     public final int maxWorldCol = 20;
     public final int maxWorldRow = 20;
     public int maxMap = 99;
 
     final int FPS = 60;
 
-//    SYSTEM
+    //    SYSTEM
     TileManager tm = new TileManager(this);
     KeyHandler keyH = new KeyHandler(this);
     Thread gameThread;
     AssetSetter aSetter = new AssetSetter(this);
-    CollisionChecker collisionChecker = new CollisionChecker(this);
+    public CollisionChecker collisionChecker = new CollisionChecker(this);
     Sound music = new Sound(); // Created 2 different objects for Sound Effect and Music. If you use 1 object SE or Music stops sometimes.
+    public UI ui = new UI(this);
 
-//    ENTITIES
-public Player player = new Player(this, keyH);
+    //    ENTITIES
+    public Player player = new Player(this, keyH);
     Entity npc[] = new Entity[10];
     Object item[] = new Object[10];
 
-//    Main.UI
-    DialogBox dbox = new DialogBox(this);
+    //    Main.UI
+//    DialogBox dbox = new DialogBox(this);
 
-//    Game state
+    //    Game state
     public int gameState;
     public final int TITLE = 0;
     public final int PLAY = 1;
@@ -60,15 +61,15 @@ public Player player = new Player(this, keyH);
         aSetter.setNPC();
 
         gameState = PLAY;
-        playMusic(0);
+//        playMusic(0);
     }
 
-    public void playMusic(int i)
-    {
+    public void playMusic(int i) {
         music.setFile(i);
         music.play();
         music.loop();
     }
+
     public void stopMusic() {
         music.stop();
     }
@@ -81,7 +82,7 @@ public Player player = new Player(this, keyH);
     @Override
     public void run() {
 
-        double drawInterval = 1000000000.0/ FPS;
+        double drawInterval = 1000000000.0 / FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -111,6 +112,12 @@ public Player player = new Player(this, keyH);
 
     public void update() {
         player.update();
+
+        for (int i = 0; i < npc.length; i++) {
+            if (npc[i] != null) {
+                npc[i].update();
+            }
+        }
     }
 
 
@@ -118,14 +125,18 @@ public Player player = new Player(this, keyH);
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
+//        tile drawing
+        tm.draw(g2);
+
+//        npc drawing
         for (int i = 0; i < npc.length; i++) {
             if (npc[i] != null) {
                 npc[i].draw(g2);
             }
         }
 
-        tm.draw(g2);
+//        player drawing
         player.draw(g2);
-        dbox.drawDialogBox(g2);
+        ui.draw(g2);
     }
 }
