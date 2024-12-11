0f6414b41cfc395b3ebe561c641526176b01e47a
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-12-11 21:02:15 +0700

    refactor: added currentMap variable

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -15,18 +15,18 @@ public class AssetSetter {
 //       set each npc and store into gamepanels' npc[] array
         int mapNum = 0;
         int i = 0;
-        gp.npc[i] = new NPC_Dukun(gp);
-        gp.npc[i].worldX = gp.tileSize * 3;
-        gp.npc[i].worldY = gp.tileSize * 3;
+        gp.npc[mapNum][i] = new NPC_Dukun(gp);
+        gp.npc[mapNum][i].worldX = gp.tileSize * 24;
+        gp.npc[mapNum][i].worldY = gp.tileSize * 29;
         i++;
     }
 
     public void setItem() {
         int mapNum = 0;
         int i = 0;
-        gp.items[i] = new I_Bowl(gp);
-        gp.items[i].worldX = gp.tileSize * 6;
-        gp.items[i].worldY = gp.tileSize * 6;
+//        gp.items[mapNum][i] = new I_Bowl(gp);
+//        gp.items[mapNum][i].worldX = gp.tileSize * 23;
+//        gp.items[mapNum][i].worldY = gp.tileSize * 4;
         i++;
     }
 
@@ -34,21 +34,21 @@ public class AssetSetter {
         int mapNum = 0;
         int i = 0;
 
-        gp.monsters[i] = new MON_tuyul(gp);
-        gp.monsters[i].worldX = gp.tileSize * 4;
-        gp.monsters[i].worldY = gp.tileSize * 4;
+//        gp.monsters[mapNum][i] = new MON_tuyul(gp);
+//        gp.monsters[mapNum][i].worldX = gp.tileSize * 16;
+//        gp.monsters[mapNum][i].worldY = gp.tileSize * 10;
         i++;
 
-        gp.monsters[i] = new MON_tuyul(gp);
-        gp.monsters[i].direction = "up";
-        gp.monsters[i].worldX = gp.tileSize * 4;
-        gp.monsters[i].worldY = gp.tileSize * 8;
+//        gp.monsters[mapNum][i] = new MON_tuyul(gp);
+//        gp.monsters[mapNum][i].direction = "up";
+//        gp.monsters[mapNum][i].worldX = gp.tileSize * 16;
+//        gp.monsters[mapNum][i].worldY = gp.tileSize * 10;
         i++;
 
-        gp.monsters[i] = new MON_tuyul(gp);
-        gp.monsters[i].direction = "left";
-        gp.monsters[i].worldX = gp.tileSize * 9;
-        gp.monsters[i].worldY = gp.tileSize * 3;
+//        gp.monsters[mapNum][i] = new MON_tuyul(gp);
+//        gp.monsters[mapNum][i].direction = "left";
+//        gp.monsters[mapNum][i].worldX = gp.tileSize * 16;
+//        gp.monsters[mapNum][i].worldY = gp.tileSize * 10;
         i++;
     }
 }
